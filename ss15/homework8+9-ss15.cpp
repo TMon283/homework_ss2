73ce742dfcbeb6415261419b949b5de6c544eedf
TMon283 <lemonboy2k6@gmail.com>
2024-12-13 07:41:19 +0700

    Update homework8+9-ss15.cpp

--- ss15/homework8+9-ss15.cpp
+++ ss15/homework8+9-ss15.cpp
@@ -2,23 +2,26 @@
 #include<string.h>
 #include<ctype.h>
 int main(){
+	int i, j;
 	char deleteIndex;
 	char string[100]; 
 	printf("Nhap chuoi bat ki: ");
 	fgets(string, sizeof(string), stdin);
 	printf("Moi ban nhap ki tu muon xoa: ");
 	scanf("%c", &deleteIndex);
-	long a = strchr(string, deleteIndex) - &string[0];
-	for(int i = a; i < strlen(string); i++){
-		string[i] = string[i + 1];
+	for(i = 0, j = 0; i < strlen(string); i++){
+		if(string[i] != deleteIndex){
+			string[j++] = string[i];
+		}
 	}
-	for(int i = 0; i < strlen(string) - 1; i++){
+	string[j] = '\0';
+	for(i = 0; i < strlen(string) - 1; i++){
 		string[0] = toupper(string[0]);
 		if(string[i] == ' '){
 			string[i + 1] = toupper(string[i + 1]);
 		}
 	}
-	for(int i = 0; i < strlen(string) - 1; i++){
+	for(i = 0; i < strlen(string) - 1; i++){
 		printf("%c", string[i]);
 	}
 	return 0;
