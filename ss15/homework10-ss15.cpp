75f9a3af6291486afc844c235f6059c2df660f13
TMon283 <lemonboy2k6@gmail.com>
2024-12-13 18:44:23 +0700

    Update homework10-ss15.cpp

--- ss15/homework10-ss15.cpp
+++ ss15/homework10-ss15.cpp
@@ -1,10 +1,33 @@
 #include<stdio.h>
 #include<string.h>
+int findIndex(char letter, char letters[]);
 int main(){
-	char string1[100];
-	char string2[] = "hello world";
-	char character = 'r';
-	char *position= strchr(string2, character);
-	printf("Dia chi cua ki tu r trong chuoi string = %lu\n", (unsigned long)&string2[0]);
+	char string[] = "hello world";
+	int length = (int)strlen(string);
+	char letters[length];
+	int times[length];
+	int count = 0;
+	for(int i = 0; i < length; i++){
+		if(findIndex(string[i], letters) == -1){
+			letters[count] = string[i];
+			times[count] = 1;
+			count++;
+		}else{
+			int index = findIndex(string[i], letters);
+			times[index]++;
+		}
+	}
+	for(int i = 0; i < count; i++){
+		printf("%c - %d\n", letters[i], times[i]);
+	}
 	return 0;
+}
+int findIndex(char letter, char letters[]){
+	int length = (int)strlen(letters);
+	for(int i = 0; i < length; i++){
+		if(letter == letters[i]){
+			return i;
+		}
+	}
+	return -1;
 }
